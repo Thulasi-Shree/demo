c7ab5f40e6b8158f4c92cffbdc0ca56993400d71
thulasishreeg <108360786+Thulasi-Shree@users.noreply.github.com>
2024-07-28 17:08:35 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -67,7 +67,7 @@ app.use(morgan('combined'));
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../frontend/index.html'))
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
 app.use(errorMiddleware)
