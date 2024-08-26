2fb958141d93f1566b786e24dc06841ab927d6e2
thulasishreeg <108360786+Thulasi-Shree@users.noreply.github.com>
2024-08-26 21:11:29 +0530

    modified

--- frontend/src/pages/user/CustomerList.js
+++ frontend/src/pages/user/CustomerList.js
@@ -250,10 +250,10 @@ const CustomerList = () => {
 
       if (searchInput) {
         response = await axios.get(
-          `/api/admin/users?&keyword=${searchInput}&page=${currentPage}`
+          `/api/admin/users/get?&keyword=${searchInput}&page=${currentPage}`
         );
       } else {
-        response = await axios.get(`/api/admin/users?&page=${currentPage}`);
+        response = await axios.get(`/api/admin/users/get?&page=${currentPage}`);
       }
 
       setResPerPage(response.data.resPerPage);
