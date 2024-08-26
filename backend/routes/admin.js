2515e23cd304f2306b7be565a40aad89a27c9907
thulasishreeg <108360786+Thulasi-Shree@users.noreply.github.com>
2024-08-26 21:35:17 +0530

    updated

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,7 +5,7 @@ const adminController = require('../controllers/admin/index');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 
 // Get All Users for Admin: GET /api/admin/users
-router.get('/admin/users/get', isAuthenticatedUser, authorizeRoles('superAdmin'), adminController.getAllUsers );
+router.get('/admin/users/get', isAuthenticatedUser, authorizeRoles(''), adminController.getAllUsers );
   
 
 // Get All Users for Admin: GET /api/admin/users
